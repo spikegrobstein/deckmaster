6fd768d517be20e4b5fd58ec066f004781263813
Spike Grobstein <spikegrobstein@mac.com>
2011-04-03 23:04:20 -0400

    improving the styling and layout of the card searcher

--- app/controllers/autocompletions_controller.rb
+++ app/controllers/autocompletions_controller.rb
@@ -4,7 +4,20 @@ class AutocompletionsController < ApplicationController
     cards = Card.find(:all, :limit => 10, :conditions => "name like '#{params[:cardname]}%'", :group => :name)
     
     respond_to do |format|
-      format.json { render :json => cards.collect { |c| [c.name, c.multiverse_id] } }
+      format.json do
+        card_info = cards.collect do |c| 
+          { 
+            :name => c.name, 
+            :multiverse_id => c.multiverse_id,
+            :casting_cost => c.casting_cost,
+            :card_type => c.card_type,
+            :power => c.power,
+            :toughness => c.toughness
+          }
+        end
+        
+        render :json => card_info
+      end
     end
   end
   
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,50 +5,62 @@ var last_search = '';
 var search_in_progress = null;
 var queued_search = null;
 
-function do_search() {
-	var search_field = $('#cardname');
-	
-		if ($(search_field).val() == last_search) { return; }
-		
-		if (search_in_progress) {
-			queued_search = $(search_field).val();
-			return;
-		}
-		
-		last_search = $(search_field).val();
-		search_in_progress = true;
-		
-		$.ajax({
-		    url: '/autocompletions',
-		    type: 'POST',
-				dataType: 'json',
-		    data: { 'cardname': $(search_field).val() },
-		    success: function(data) {
-		        var results = $('#autocomplete_results');
-						
-						if (data.length <= 1) { return; }
-						
-						results.html(''); // clear that shit
-						
-						$(data).each(function(r) {
-							results.append("<li>" + data[r][0] + " - " + data[r][1] + "</li>")
-						});
-		    },
-				complete: function() {
-					search_in_progress = false;
-					if (queued_search) {
-						$(search_field).keydown();
-						queued_search = null;
-					}
-				}
-		});
-}
-
 $(function() {
+	// build some templates
+	
+	$.template(
+		'search_result', 
+		'<li>\
+			<div>\
+				<a class="card_name" href="#" rel="/cards/${multiverse_id}">${name}</a>\
+				<span class="casting_cost">${casting_cost}</span>\
+			</div>\
+			<div>\
+				<span class="type">${card_type}</span>\
+				<span class="power_toughness">${power}/${toughness}\
+			</div>\
+		</li>'
+	);
+	
 	$('#cardname').keydown(function(event) {
 		// real-time searching of cards
 		
-		setTimeout(do_search, 250);
+		setTimeout(function() {
+			var search_field = $('#cardname');
+
+				if ($(search_field).val() == last_search) { return; }
+
+				if (search_in_progress) {
+					queued_search = $(search_field).val();
+					return;
+				}
+
+				last_search = $(search_field).val();
+				search_in_progress = true;
+
+				$.ajax({
+				    url: '/autocompletions',
+				    type: 'POST',
+						dataType: 'json',
+				    data: { 'cardname': $(search_field).val() },
+				    success: function(data) {
+				        var results = $('#autocomplete_results');
+
+								if (data.length <= 1) { return; }
+
+								results.html(''); // clear that shit
+
+								$.tmpl('search_result', data).appendTo('#autocomplete_results');
+				    },
+						complete: function() {
+							search_in_progress = false;
+							if (queued_search) {
+								$(search_field).keydown();
+								queued_search = null;
+							}
+						}
+				});
+		}, 250);
 	});
 });
 
