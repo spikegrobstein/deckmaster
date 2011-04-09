d9b9779834ccf1e123003e815e3f61c67e9c4883
Spike Grobstein <spikegrobstein@mac.com>
2011-04-09 15:53:30 -0400

    no more default scope on DeckCard

--- app/models/deck_card.rb
+++ app/models/deck_card.rb
@@ -1,9 +1,7 @@
 class DeckCard < ActiveRecord::Base
   belongs_to :deck
   belongs_to :card
-  
-  default_scope :select => 'card_id, deck_id'
-  
+    
   def multiverse_id=(mid)
     self.card = Card.find_by_multiverse_id(mid)
   end
