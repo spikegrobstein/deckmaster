70ec3fb347e9966a3baebdff4417a8b22dbfd4d9
Spike Grobstein <spikegrobstein@mac.com>
2011-04-09 23:49:40 -0400

    increased limit to 20

--- app/controllers/autocompletions_controller.rb
+++ app/controllers/autocompletions_controller.rb
@@ -1,7 +1,7 @@
 class AutocompletionsController < ApplicationController
   
   def create
-    cards = Card.find(:all, :limit => 10, :conditions => "name like '#{params[:cardname]}%'", :group => :name)
+    cards = Card.find(:all, :limit => 20, :conditions => "name like '#{params[:cardname]}%'", :group => :name)
     
     respond_to do |format|
       format.json do
